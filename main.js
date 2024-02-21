96038fcd5103998148c8d6fb745be2614391432b
noumecha <noumechaivan@gmail.com>
2024-02-22 00:43:14 +0100

    first fly in app html

---
+++ main.js
@@ -0,0 +1,26 @@
+// importing electron module to the project
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on("activate", () => {
+        if(BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
