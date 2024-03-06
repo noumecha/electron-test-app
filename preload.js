f3c5ee8d28a76cd640e5191411a8abbd36e4a74c
noumecha <noumechaivan@gmail.com>
2024-03-06 01:52:49 +0100

    maj

--- preload.js
+++ preload.js
@@ -4,5 +4,5 @@ contextBridge.exposeInMainWorld('versions', {
     node: process.versions.node,
     chrome: process.versions.chrome,
     electron: process.versions.electron,
-    ping: ipcRenderer.invoke('ping')
+    ping: () => ipcRenderer.invoke('ping')
 })
